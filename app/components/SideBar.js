15dcbe1bb77a6d004af055d81bd3a59510607059
cdiezmoran <cdiezmoran@gmail.com>
2017-09-05 00:32:11 -0500

    Add window controls to sidebar

--- app/components/SideBar.js
+++ app/components/SideBar.js
@@ -1,12 +1,36 @@
 // @flow
 import React, { Component } from 'react';
+import { remote } from 'electron';
 import styles from './SideBar.scss';
 
 export default class SideBar extends Component {
+  handleMaximize = () => {
+    const win = remote.getCurrentWindow();
+    if (!win.isMaximized()) {
+      win.maximize();
+    } else {
+      win.unmaximize();
+    }
+  }
+
+  handleMinimize = () => {
+    const win = remote.getCurrentWindow();
+    win.minimize();
+  }
+
+  handleClose = () => {
+    const win = remote.getCurrentWindow();
+    win.close();
+  }
+
   render() {
     return (
       <div className={styles.Menu}>
-        <p>menu</p>
+        <div className={styles.Controls}>
+          <button className={styles.CloseControl} onClick={this.handleClose} />
+          <button className={styles.MinimizeControl} onClick={this.handleMinimize} />
+          <button className={styles.MaximizeControl} onClick={this.handleMaximize} />
+        </div>
       </div>
     );
   }
