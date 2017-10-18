6cd489189606bfe504504abd40baf16c2dad43ee
cdiezmoran <cdiezmoran@gmail.com>
2017-10-18 14:54:49 -0500

    Switch to controlled inputs for auth

--- app/components/Auth/Login.js
+++ app/components/Auth/Login.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
-const Login = ({ switchForm }) => (
-  <div className={styles.Login}>
-    <div className={styles.InputGroup}>
-      <label htmlFor="loginEmail" className={styles.Tag}>EMAIL</label>
-      <input id="loginEmail" type="email" className={styles.Input} />
-    </div>
-    <div className={styles.InputGroup}>
-      <label htmlFor="password" className={styles.Tag}>PASSWORD</label>
-      <input id="password" type="password" className={styles.Input} />
-    </div>
+class Login extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
 
-    <button className={styles.SubmitButton}>Login</button>
+  handleChange = (event) => {
+    const { name, value } = event.target;
 
-    <span className={styles.Switch}>
-      New to Alpha Stage? <button onClick={switchForm}>Create an account!</button>
-    </span>
-  </div>
-);
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { switchForm } = this.props;
+    const { email, password } = this.state;
+    console.log(email, password);
+    return (
+      <div className={styles.Login}>
+        <div className={styles.InputGroup}>
+          <label htmlFor="loginEmail" className={styles.Tag}>EMAIL</label>
+          <input
+            id="loginEmail"
+            type="email"
+            name="email"
+            className={styles.Input}
+            value={email}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className={styles.InputGroup}>
+          <label htmlFor="password" className={styles.Tag}>PASSWORD</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={styles.Input}
+            value={password}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <button className={styles.SubmitButton}>Login</button>
+
+        <span className={styles.Switch}>
+          New to Alpha Stage? <button onClick={switchForm}>Create an account!</button>
+        </span>
+      </div>
+    );
+  }
+}
 
 Login.propTypes = {
   switchForm: PropTypes.func.isRequired
--- app/components/Auth/Signup.js
+++ app/components/Auth/Signup.js
@@ -1,33 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
-const Signup = ({ switchForm }) => (
-  <div className={styles.Signup}>
-    <div className={styles.InputGroup}>
-      <label htmlFor="loginEmail" className={styles.Tag}>EMAIL</label>
-      <input id="loginEmail" type="email" className={styles.Input} />
-    </div>
-    <div className={styles.InputGroup}>
-      <label htmlFor="username" className={styles.Tag}>USERNAME</label>
-      <input id="username" type="text" className={styles.Input} />
-    </div>
-    <div className={styles.InputGroup}>
-      <label htmlFor="password" className={styles.Tag}>PASSWORD</label>
-      <input id="password" type="password" className={styles.Input} />
-    </div>
-    <div className={styles.InputGroup}>
-      <label htmlFor="confirmPassword" className={styles.Tag}>CONFIRM PASSWORD</label>
-      <input id="confirmPassword" type="password" className={styles.Input} />
-    </div>
+class Signup extends Component {
+  state = {
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: ''
+  }
 
-    <button className={styles.SubmitButton}>Signup</button>
+  handleChange = (event) => {
+    const { name, value } = event.target;
 
-    <span className={styles.Switch}>
-      Already have an account? <button onClick={switchForm}>Login now!</button>
-    </span>
-  </div>
-);
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { switchForm } = this.props;
+    const { email, username, password, confirmPassword } = this.state;
+    return (
+      <div className={styles.Signup}>
+        <div className={styles.InputGroup}>
+          <label htmlFor="loginEmail" className={styles.Tag}>EMAIL</label>
+          <input
+            id="loginEmail"
+            type="email"
+            name="email"
+            className={styles.Input}
+            value={email}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className={styles.InputGroup}>
+          <label htmlFor="username" className={styles.Tag}>USERNAME</label>
+          <input
+            id="username"
+            type="text"
+            name="username"
+            className={styles.Input}
+            value={username}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className={styles.InputGroup}>
+          <label htmlFor="password" className={styles.Tag}>PASSWORD</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={styles.Input}
+            value={password}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className={styles.InputGroup}>
+          <label htmlFor="confirmPassword" className={styles.Tag}>CONFIRM PASSWORD</label>
+          <input
+            id="confirmPassword"
+            type="password"
+            name="confirmPassword"
+            className={styles.Input}
+            value={confirmPassword}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <button className={styles.SubmitButton}>Signup</button>
+
+        <span className={styles.Switch}>
+          Already have an account? <button onClick={switchForm}>Login now!</button>
+        </span>
+      </div>
+    );
+  }
+}
 
 Signup.propTypes = {
   switchForm: PropTypes.func.isRequired
