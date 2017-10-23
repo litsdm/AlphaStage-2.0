7e6641e445f5c3f9f66a77e68ccba1d9fbc28190
cdiezmoran <cdiezmoran@gmail.com>
2017-10-22 22:45:46 -0500

    Add Menu with styles

--- app/components/SideBar/SideBar.js
+++ app/components/SideBar/SideBar.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import styles from './SideBar.scss';
 
 import Profile from './Profile';
+import Menu from './Menu';
 
 class SideBar extends Component {
   handleMaximize = () => {
@@ -29,12 +30,13 @@ class SideBar extends Component {
   render() {
     const { user, logout, updateUserPic } = this.props;
     return (
-      <div className={styles.Menu}>
+      <div className={styles.SideBar}>
         <div className={styles.Controls}>
           <button className={styles.CloseControl} onClick={this.handleClose} />
           <button className={styles.MinimizeControl} onClick={this.handleMinimize} />
           <button className={styles.MaximizeControl} onClick={this.handleMaximize} />
         </div>
+        <Menu />
         <Profile user={user} logout={logout} updateUserPic={updateUserPic} />
       </div>
     );
