847f105e87389427cbd428dbd758a66198b782de
cdiezmoran <cdiezmoran@gmail.com>
2017-10-24 14:49:23 -0500

    Make controlled inputs for basic

--- app/components/CreateGame/Basic.js
+++ app/components/CreateGame/Basic.js
@@ -1,30 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
-class Basic extends Component {
-  render() {
-    return (
-      <div className={styles.Row}>
-        <div className={styles.ColumnLeft}>
-          <p className={styles.Title}>Basic Information</p>
-        </div>
-        <div className={styles.ColumnRight}>
-          <div className={styles.InputContainer}>
-            <label htmlFor="title" className={styles.Tag}>Title</label>
-            <input type="text" id="title" name="title" className={styles.Input} />
-          </div>
-          <div className={styles.InputContainer}>
-            <label htmlFor="shortDescription" className={styles.Tag}>Short Description</label>
-            <input type="text" id="shortDescription" name="shortDescription" className={styles.Input} />
-          </div>
-          <div className={styles.InputContainer}>
-            <label htmlFor="releaseStatus" className={styles.Tag}>Release Status</label>
-            <input type="text" id="releaseStatus" name="releaseStatus" className={styles.Input} />
-          </div>
-        </div>
+const Basic = ({ title, shortDescription, releaseStatus, handleChange }) => (
+  <div className={styles.Row}>
+    <div className={styles.ColumnLeft}>
+      <p className={styles.Title}>Basic Information</p>
+    </div>
+    <div className={styles.ColumnRight}>
+      <div className={styles.InputContainer}>
+        <label htmlFor="title" className={styles.Tag}>Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          className={styles.Input}
+          value={title}
+          onChange={handleChange}
+        />
       </div>
-    );
-  }
-}
+      <div className={styles.InputContainer}>
+        <label htmlFor="shortDescription" className={styles.Tag}>Short Description</label>
+        <input
+          type="text"
+          id="shortDescription"
+          name="shortDescription"
+          className={styles.Input}
+          value={shortDescription}
+          onChange={handleChange}
+        />
+      </div>
+      <div className={styles.InputContainer}>
+        <label htmlFor="releaseStatus" className={styles.Tag}>Release Status</label>
+        <input
+          type="text"
+          id="releaseStatus"
+          name="releaseStatus"
+          className={styles.Input}
+          value={releaseStatus}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  </div>
+);
+
+Basic.propTypes = {
+  title: PropTypes.string.isRequired,
+  shortDescription: PropTypes.string.isRequired,
+  releaseStatus: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
+};
 
 export default Basic;
--- app/components/CreateGame/CreateGame.js
+++ app/components/CreateGame/CreateGame.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles.scss';
 
 import Basic from './Basic';
 
-const CreateGame = () => (
-  <div className={styles.CreateGame}>
-    <Basic />
-    <div className={styles.Divider} />
-  </div>
-);
+class CreateGame extends Component {
+  state = {
+    title: '',
+    shortDescription: '',
+    releaseStatus: ''
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { title, shortDescription, releaseStatus } = this.state;
+    console.log(this.state);
+    return (
+      <div className={styles.CreateGame}>
+        <Basic
+          title={title}
+          shortDescription={shortDescription}
+          releaseStatus={releaseStatus}
+          handleChange={this.handleChange}
+        />
+        <div className={styles.Divider} />
+      </div>
+    );
+  }
+}
 
 export default CreateGame;
