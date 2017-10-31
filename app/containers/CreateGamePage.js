c29c3dd61f7a98196ca206e8859b1711d748d2a2
cdiezmoran <cdiezmoran@gmail.com>
2017-10-31 14:58:46 -0600

    Add graphql mutation

--- app/containers/CreateGamePage.js
+++ app/containers/CreateGamePage.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import createGame from '../graphql/createGame.graphql';
 
 import CreateGame from '../components/CreateGame/CreateGame';
 
-const CreateGamePage = () => (
+const mapStateToProps = ({ user }) => (
+  {
+    user
+  }
+);
+
+const withMutation = graphql(createGame, {
+  props: ({ mutate }) => ({
+    submitGame: (input) => mutate({ variables: { input } }),
+  }),
+});
+
+const CreateGamePage = ({ submitGame, user }) => (
   <div>
-    <CreateGame />
+    <CreateGame submitGame={submitGame} user={user} />
   </div>
 );
 
-export default CreateGamePage;
+CreateGamePage.propTypes = {
+  submitGame: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired
+};
+
+const connectedComponent = connect(mapStateToProps, null)(CreateGamePage);
+
+export default withMutation(connectedComponent);
