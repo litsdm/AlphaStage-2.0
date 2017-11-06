93971af0ce4eca62b30061e201d9e90342da2be8
cdiezmoran <cdiezmoran@gmail.com>
2017-11-06 13:27:44 -0600

    Refactor additional details row

--- app/components/GameShow/AdditionalDetails.js
+++ app/components/GameShow/AdditionalDetails.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import styles from './AdditionalDetails.scss';
 
+import DetailsRow from './DetailsRow';
+
 const AdditionalDetails = () => (
   <div className={styles.AdditionalDetails}>
     <p className={styles.Title}>Additional Details</p>
-    <div className={styles.Row}>
-      <p className={styles.Subtitle}>Languages</p>
-      <div className={styles.Values}>
-        <p>English</p>
-        <p>Spanish</p>
-        <p>Japanese</p>
-      </div>
-    </div>
-    <div className={styles.Row}>
-      <p className={styles.Subtitle}>Publisher</p>
-      <div className={styles.Values}>
-        <p>Alpha Stage Studios</p>
-      </div>
-    </div>
-    <div className={styles.Row}>
-      <p className={styles.Subtitle}>Space Required</p>
-      <div className={styles.Values}>
-        <p>20 GB</p>
-      </div>
-    </div>
-    <div className={styles.Row}>
-      <p className={styles.Subtitle}>Version</p>
-      <div className={styles.Values}>
-        <p>0.0.1</p>
-      </div>
-    </div>
+    <DetailsRow subtitle={'Languages'} values={['English', 'Spanish', 'Japanese']} />
+    <DetailsRow subtitle={'Publisher'} values={['Alpha Stage Studios']} />
+    <DetailsRow subtitle={'Space Required'} values={['20 GB']} />
+    <DetailsRow subtitle={'Version'} values={['0.0.1']} />
   </div>
 );
 
