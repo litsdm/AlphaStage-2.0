b376cf896ada6af077eafd85949dac52b2e85141
cdiezmoran <cdiezmoran@gmail.com>
2017-11-10 00:33:45 -0600

    Add state to game page

--- app/containers/GamePage.js
+++ app/containers/GamePage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import GameShow from '../components/GameShow/GameShow';
 import Loader from '../components/Loader';
 
 import fullGameQuery from '../graphql/fullGame.graphql';
+
+const mapStateToProps = ({ game }) => (
+  {
+    ...game
+  }
+);
 
 const withGame = graphql(fullGameQuery, {
   props: ({ data }) => {
@@ -18,22 +25,31 @@ const withGame = graphql(fullGameQuery, {
   options: (props) => ({ variables: { id: props.match.params.id } })
 });
 
-const GamePage = ({ game, loading }) => (
+const GamePage = ({ game, loading, isDownloading, isInstalling }) => (
   loading
   ? <Loader />
-  : <GameShow game={game} />
+  : <GameShow
+    game={game}
+    isDownloading={isDownloading}
+    isInstalling={isInstalling}
+  />
 );
 
 GamePage.propTypes = {
   loading: PropTypes.bool,
   game: PropTypes.object,
+  isDownloading: PropTypes.bool,
+  isInstalling: PropTypes.bool
 };
 
 GamePage.defaultProps = {
   loading: false,
-  game: {}
+  game: {},
+  isDownloading: false,
+  isInstalling: false
 };
 
-const GamePageWithData = withGame(GamePage);
+const GamePageWithProps = connect(mapStateToProps, null)(GamePage);
+const GamePageWithData = withGame(GamePageWithProps);
 
 export default GamePageWithData;
