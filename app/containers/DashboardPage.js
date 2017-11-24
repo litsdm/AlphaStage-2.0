48ee9c773f207f550db1fa0e96c79f038cb32c90
cdiezmoran <cdiezmoran@gmail.com>
2017-11-23 22:16:08 -0600

    Add data to dashboard page

--- app/containers/DashboardPage.js
+++ app/containers/DashboardPage.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import jwtDecode from 'jwt-decode';
 
 import Dashboard from '../components/Dashboard/Dashboard';
 
-const DashboardPage = () => (
+import userGamesQuery from '../graphql/userGames.graphql';
+
+const mapStateToProps = ({ user }) => (
+  {
+    user
+  }
+);
+
+const withGames = graphql(userGamesQuery, {
+  props: ({ data }) => {
+    if (!data.user) return { loading: data.loading };
+    if (data.error) return { hasErrors: true };
+    return {
+      games: data.user.games,
+    };
+  },
+  options: () => {
+    const token = localStorage.getItem('token');
+    const user = jwtDecode(token);
+    return { variables: { id: user._id } };
+  }
+});
+
+const DashboardPage = ({ user, games }) => (
   <div>
-    <Dashboard />
+    <Dashboard user={user} games={games} />
   </div>
 );
 
-export default DashboardPage;
+DashboardPage.propTypes = {
+  user: PropTypes.object.isRequired,
+  games: PropTypes.array.isRequired
+};
+
+const DashboardWithProps = connect(mapStateToProps, null)(DashboardPage);
+const DashboardWithGames = withGames(DashboardWithProps);
+
+export default DashboardWithGames;
