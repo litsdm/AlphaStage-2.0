1dd624f0d9026f2e13dec35406257be084f8bf79
cdiezmoran <cdiezmoran@gmail.com>
2017-11-27 21:13:49 -0600

    Make header its own component

--- app/components/Dashboard/Dashboard.js
+++ app/components/Dashboard/Dashboard.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
-import Dropdown from './Dropdown';
+import Header from './Header';
 
 class Dashboard extends Component {
   state = {
-    selectedIndex: 0,
+    currentIndex: 0,
     displayDropdown: false,
     tabIndex: 0
   }
@@ -17,7 +17,7 @@ class Dashboard extends Component {
 
   selectGame = (index) => () => {
     this.setState({
-      selectedIndex: index,
+      currentIndex: index,
       displayDropdown: false
     });
   }
@@ -28,50 +28,19 @@ class Dashboard extends Component {
 
   render() {
     const { games } = this.props;
-    const { selectedIndex, displayDropdown, tabIndex } = this.state;
-    const selectedGame = games[selectedIndex];
+    const { currentIndex, displayDropdown, tabIndex } = this.state;
 
     return (
       <div className={styles.Dashboard}>
-        <div className={styles.Header} style={{ backgroundImage: `url(${selectedGame.coverImage})` }}>
-          <div className={styles.Overlay} />
-          <div className={styles.TitleContainer}>
-            <p className={styles.Title}>{selectedGame.title}</p>
-            <button onClick={this.toggleDropdown}>
-              <i className={`fa ${displayDropdown ? 'fa-times' : 'fa-chevron-down'}`} />
-            </button>
-          </div>
-          <Dropdown
-            games={games}
-            selectedIndex={selectedIndex}
-            display={displayDropdown}
-            selectGame={this.selectGame}
-          />
-          <div className={styles.Menu}>
-            <button
-              className={`${styles.Tab} ${tabIndex === 0 ? styles.active : ''}`}
-              onClick={this.selectTab(0)}
-            >
-              Overview
-              <div className={styles.Indicator} />
-            </button>
-            <button
-              className={`${styles.Tab} ${tabIndex === 1 ? styles.active : ''}`}
-              onClick={this.selectTab(1)}
-            >
-              Feedback
-              <div className={styles.Indicator} />
-            </button>
-          </div>
-          <div className={styles.ConfigButtons}>
-            <button>
-              <i className="fa fa-pencil" />
-            </button>
-            <button>
-              <i className="fa fa-cog" />
-            </button>
-          </div>
-        </div>
+        <Header
+          games={games}
+          currentIndex={currentIndex}
+          displayDropdown={displayDropdown}
+          tabIndex={tabIndex}
+          toggleDropdown={this.toggleDropdown}
+          selectGame={this.selectGame}
+          selectTab={this.selectTab}
+        />
       </div>
     );
   }
--- app/components/Dashboard/Dropdown.js
+++ app/components/Dashboard/Dropdown.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import uuid from 'uuid/v4';
 import styles from './Dropdown.scss';
 
-const Dropdown = ({ games, selectedIndex, display, selectGame }) => {
+const Dropdown = ({ games, currentIndex, display, selectGame }) => {
   const renderRows = () => (
     games.map((game, i) => (
       <button key={uuid()} className={styles.Row} onClick={selectGame(i)}>
         <img src={game.thumbnail} alt="game's thumbnail in game selection" />
         <p>{game.title}</p>
-        {selectedIndex === i ? <i className="fa fa-check" /> : null}
+        {currentIndex === i ? <i className="fa fa-check" /> : null}
       </button>
     ))
   );
@@ -26,14 +26,14 @@ const Dropdown = ({ games, selectedIndex, display, selectGame }) => {
 
 Dropdown.propTypes = {
   games: PropTypes.array,
-  selectedIndex: PropTypes.number,
+  currentIndex: PropTypes.number,
   display: PropTypes.bool,
   selectGame: PropTypes.func.isRequired
 };
 
 Dropdown.defaultProps = {
   games: [],
-  selectedIndex: 0,
+  currentIndex: 0,
   display: false
 };
 
