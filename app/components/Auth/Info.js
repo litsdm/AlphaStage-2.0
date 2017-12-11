74ad2d085fd64867a5050ddaba9b7e9ab3bb63b5
cdiezmoran <cdiezmoran@gmail.com>
2017-12-11 02:04:42 -0600

    Change and add logo to info

--- app/components/Auth/Info.js
+++ app/components/Auth/Info.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
+import logo from '../../../resources/icon.png';
 
 const Info = ({ message, isNewUser }) => (
   <div className={[styles.Info, isNewUser ? styles.NewUser : ''].join(' ')}>
+    <img src={logo} alt="logo" style={{ width: '100px' }} />
     <h2>{message}</h2>
   </div>
 );
