d4040628cb6821f76aa29044a74b6947f6ac5ada
cdiezmoran <cdiezmoran@gmail.com>
2017-12-12 15:07:52 -0600

    Add remaining icons to iconsys

--- app/components/IconSystem/Icon.js
+++ app/components/IconSystem/Icon.js
@@ -8,7 +8,19 @@ import DrivingWheel from './Icons/DrivingWheel';
 import Flask from './Icons/Flask';
 import Gamepad from './Icons/Gamepad';
 import Heart from './Icons/Heart';
+import Idea from './Icons/Idea';
+import Invader from './Icons/Invader';
+import Joystick from './Icons/Joystick';
+import Letter from './Icons/Letter';
 import Map from './Icons/Map';
+import Rifle from './Icons/Rifle';
+import Rocket from './Icons/Rocket';
+import Sniper from './Icons/Sniper';
+import Soccer from './Icons/Soccer';
+import Strategy from './Icons/Strategy';
+import SwordShield from './Icons/SwordShield';
+import Tank from './Icons/Tank';
+import Whistle from './Icons/Whistle';
 
 const defaultStyles = {
   opacity: '0.3',
@@ -27,7 +39,19 @@ const Icon = ({ title, styles, fill }) => {
     flask: <Flask styles={combinedStyles} fill={fill} />,
     gamepad: <Gamepad styles={combinedStyles} fill={fill} />,
     heart: <Heart styles={combinedStyles} fill={fill} />,
-    map: <Map styles={combinedStyles} fill={fill} />
+    idea: <Idea styles={combinedStyles} fill={fill} />,
+    invader: <Invader styles={combinedStyles} fill={fill} />,
+    joystick: <Joystick styles={combinedStyles} fill={fill} />,
+    Letter: <Letter styles={combinedStyles} fill={fill} />,
+    map: <Map styles={combinedStyles} fill={fill} />,
+    rifle: <Rifle styles={combinedStyles} fill={fill} />,
+    rocket: <Rocket styles={combinedStyles} fill={fill} />,
+    sniper: <Sniper styles={combinedStyles} fill={fill} />,
+    soccer: <Soccer styles={combinedStyles} fill={fill} />,
+    strategy: <Strategy styles={combinedStyles} fill={fill} />,
+    swordShield: <SwordShield styles={combinedStyles} fill={fill} />,
+    tank: <Tank styles={combinedStyles} fill={fill} />,
+    whistle: <Whistle styles={combinedStyles} fill={fill} />
   };
 
   return icons[title];
