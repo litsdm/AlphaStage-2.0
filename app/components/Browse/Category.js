fbbc8441241dfe6aadf1e6f9b653b6fa3213ae86
cdiezmoran <cdiezmoran@gmail.com>
2017-12-14 02:10:26 -0600

    Add icon style picker helper

--- app/components/Browse/Category.js
+++ app/components/Browse/Category.js
@@ -6,83 +6,12 @@ import styles from './styles.scss';
 
 import Icon from '../IconSystem/Icon';
 
-const smallStyles = {
-  opacity: '0.1',
-  width: '35px',
-  height: '35px'
-};
-
-const iconStyles = [
-  {
-    top: '50px',
-    left: '35px',
-  },
-  {
-    left: '35px',
-    bottom: '50px',
-  },
-  {
-    left: '120px',
-    top: '35px',
-  },
-  {
-    left: '120px',
-    bottom: '35px',
-  },
-  {
-    top: '50px',
-    left: '35px',
-  },
-  {
-    left: '20px',
-  },
-  {
-    right: '20px',
-  },
-  {
-    right: '35px',
-    top: '50px',
-  },
-  {
-    right: '35px',
-    bottom: '50px'
-  },
-  {
-    left: '45px',
-    top: '90px',
-    ...smallStyles
-  },
-  {
-    left: '45px',
-    bottom: '90px',
-    ...smallStyles
-  },
-  {
-    rigth: '45px',
-    top: '90px',
-    ...smallStyles
-  },
-  {
-    rigth: '45px',
-    bottom: '90px',
-    ...smallStyles
-  },
-  {
-    top: '90px',
-    ...smallStyles
-  },
-  {
-    bottom: '90px',
-    ...smallStyles
-  }
-];
+import pickIconStyle from '../../helpers/pickIconStyle';
 
 const Category = ({ title, icons }) => {
   const renderIcons = () => (
     icons.map(icon => {
-      const randomIndex = Math.floor(Math.random() * iconStyles.length);
-      const style = iconStyles[randomIndex];
-      iconStyles.splice(randomIndex, 1);
+      const style = pickIconStyle(title);
       return <Icon key={uuid()} title={icon} styles={style} />;
     })
   );
@@ -91,7 +20,6 @@ const Category = ({ title, icons }) => {
     <Link
       to={`/categories/${title.toLowerCase()}`}
       className={styles.Category}
-      // style={image ? { backgroundImage: `url('${image}')` } : {}}
     >
       {renderIcons()}
       <p>{title}</p>
