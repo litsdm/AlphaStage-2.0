2958c93104d089f38e75c7d9b19114b6cfaa5dfa
cdiezmoran <cdiezmoran@gmail.com>
2017-12-17 01:28:08 +0300

    Add games by tags query to category page

---
+++ app/components/Category/Category.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Category = () => (
+  <div>
+    <p>Category</p>
+  </div>
+);
+
+export default Category;
--- app/containers/CategoryPage.js
+++ app/containers/CategoryPage.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import PropTypes from 'prop-types';
 
-const CategoryPage = () => (
-  <div>
-    <p>Category</p>
-  </div>
+import gamesByTags from '../graphql/gamesByTags.graphql';
+
+import Category from '../components/Category/Category';
+import Loader from '../components/Loader';
+
+const withGames = graphql(gamesByTags, {
+  props: ({ data }) => {
+    if (!data.gamesByTags) return { loading: data.loading };
+    if (data.error) return { hasErrors: true };
+    return {
+      games: data.gamesByTags,
+    };
+  },
+  options: (props) => ({ variables: { tags: [props.match.params.category] } })
+});
+
+const CategoryPage = ({ games, loading }) => (
+  loading
+    ? <Loader />
+    : <Category games={games} />
 );
 
-export default CategoryPage;
+CategoryPage.propTypes = {
+  loading: PropTypes.bool,
+  games: PropTypes.array,
+};
+
+CategoryPage.defaultProps = {
+  loading: false,
+  games: []
+};
+
+export default withGames(CategoryPage);
