ae3472da66837670c5698086ba150a9099b5c44a
cdiezmoran <cdiezmoran@gmail.com>
2017-12-19 02:06:19 +0300

    Add simple game grid to category page

--- app/components/Category/Category.js
+++ app/components/Category/Category.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Category = () => (
-  <div>
-    <p>Category</p>
-  </div>
+import GameGrid from './GameGrid';
+
+const Category = ({ games }) => (
+  games.length < 1
+    ? (
+      <div>
+        <p>No games found</p>
+      </div>
+    )
+    : <GameGrid games={games} />
 );
+
+Category.propTypes = {
+  games: PropTypes.array
+};
+
+Category.defaultProps = {
+  games: []
+};
 
 export default Category;
---
+++ app/components/Category/GameGrid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import uuid from 'uuid/v4';
+import PropTypes from 'prop-types';
+import styles from './GameGrid.scss';
+
+const GameGrid = ({ games }) => {
+  const renderGames = () => games.map(game => (
+    <Link to={`/games/${game._id}`} key={uuid()}>
+      <img className={styles.Thumbnail} alt="Game in category page" src={game.thumbnail} />
+    </Link>
+  ));
+
+  return (
+    <div className={styles.Grid}>
+      {renderGames()}
+    </div>
+  );
+};
+
+GameGrid.propTypes = {
+  games: PropTypes.array.isRequired
+};
+
+export default GameGrid;
