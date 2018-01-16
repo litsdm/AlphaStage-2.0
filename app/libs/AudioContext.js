a1957cdfd674bc1455bd6c41d11d9a6eed635d4a
cdiezmoran <cdiezmoran@gmail.com>
2018-01-16 14:45:01 -0600

    Create initial libraries

---
+++ app/libs/AudioContext.js
@@ -0,0 +1,15 @@
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const analyser = audioCtx.createAnalyser();
+
+const AudioContext = {
+
+  getAudioContext() {
+    return audioCtx;
+  },
+
+  getAnalyser() {
+    return analyser;
+  },
+};
+
+export default AudioContext;
