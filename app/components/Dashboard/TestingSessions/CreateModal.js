c4d966b616697bc48edd5f602853dc40f1c9aca1
cdiezmoran <cdiezmoran@gmail.com>
2018-02-06 12:39:28 -0600

    Add create modal styles

--- app/components/Dashboard/TestingSessions/CreateModal.js
+++ app/components/Dashboard/TestingSessions/CreateModal.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { DateRangePicker } from 'react-dates';
 import { string } from 'prop-types';
+import styles from './CreateModal.scss';
 
 import Modal from '../../Modal';
 
@@ -7,6 +9,7 @@ import Modal from '../../Modal';
 class Create extends Component {
   state = {
     endDate: null,
+    focusedInput: null,
     maxTesters: 0,
     rewardType: '',
     reward: '',
@@ -16,9 +19,36 @@ class Create extends Component {
   render() {
     const { id } = this.props;
     return (
-      <Modal id={id}>
-        <div>
-          Create Session Modal
+      <Modal id={id} title="Create Testing Session">
+        <div className={styles.Container}>
+          <div className={styles.InputContainer}>
+            <p>Duration</p>
+            <DateRangePicker
+              startDate={this.state.startDate}
+              startDateId="sessionStart"
+              endDate={this.state.endDate}
+              endDateId="sessionEnd"
+              onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
+              focusedInput={this.state.focusedInput}
+              onFocusChange={focusedInput => this.setState({ focusedInput })}
+            />
+          </div>
+          <div className={styles.InputContainer}>
+            <p>Number of Testers</p>
+            <input className={styles.Input} />
+          </div>
+          <div className={styles.InputContainer}>
+            <p>Reward Type</p>
+            <select className={styles.Select} />
+          </div>
+          <div className={styles.InputContainer}>
+            <p>Individual Reward</p>
+            <input className={styles.Input} />
+          </div>
+          <div className={styles.Footer}>
+            <button className={styles.Cancel}>Cancel</button>
+            <button className={styles.Submit}>Submit</button>
+          </div>
         </div>
       </Modal>
     );
