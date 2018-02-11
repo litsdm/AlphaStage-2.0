563e8adf9504fa0ee5c48bc8a714abded27f6633
cdiezmoran <cdiezmoran@gmail.com>
2018-02-11 13:50:53 -0600

    Add basic hide functionality to banner

--- app/components/Dashboard/TestingSessions/Banner.js
+++ app/components/Dashboard/TestingSessions/Banner.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Banner.scss';
 
-const Banner = () => (
-  <div className={styles.Banner}>
-    <span>
-      This game has a play testing session available.
-      <span className={styles.Eligible}>
-        {' '}You are eligible!
-      </span>
-    </span>
-    <div className={styles.Buttons}>
-      <button className={styles.Start}>Start Session</button>
-      <button className={styles.Hide}>Hide</button>
-    </div>
-  </div>
-);
+class Banner extends Component {
+  state = {
+    hide: false
+  }
+
+  onHide = () => this.setState({ hide: true });
+
+  render() {
+    const { hide } = this.state;
+    return !hide
+      ? (
+        <div className={styles.Banner}>
+          <span>
+            This game has a play testing session available.
+            <span className={styles.Eligible}>
+              {' '}You are eligible!
+            </span>
+          </span>
+          <div className={styles.Buttons}>
+            <button className={styles.Start}>Start Session</button>
+            <button className={styles.Hide} onClick={this.onHide}>Hide</button>
+          </div>
+        </div>
+      )
+      : null;
+  }
+}
 
 export default Banner;
