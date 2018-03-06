ddc5477df3df7177f78d9de7a04c791d53b0c9cf
cdiezmoran <cdiezmoran@gmail.com>
2018-03-06 16:42:45 -0600

    Add missing data to tests

--- app/components/Dashboard/TestingSessions/Tests.js
+++ app/components/Dashboard/TestingSessions/Tests.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import moment from 'moment';
 import uuid from 'uuid/v4';
+import _ from 'lodash';
 import { object } from 'prop-types';
 import styles from './Tests.scss';
 
 const DEFAULT_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
 
 const Tests = ({ session }) => {
-  const { testers, maxTesters, rewardType, reward } = session;
+  const { testers, maxTesters, rewardType, reward, tests } = session;
+
+  const formatDuration = (duration) => {
+    const sec = parseInt(duration % 60, 10);
+    const min = parseInt((duration / 60) % 60, 10);
+    return `${min}:${sec}`;
+  };
+
+  const renderTests = () =>
+    tests.map(({ createdAt, duration, testerId, completedObjectives }) => {
+      const formattedDuration = formatDuration(duration);
+
+      const date = moment(createdAt);
+      const now = moment();
+
+      const user = _.find(testers, { _id: testerId });
+      return (
+        <div className={styles.Test} key={uuid()}>
+          <p className={styles.Dates}>Created {now.to(date)}</p>
+          <p className={styles.User}>Test by {user.username}</p>
+          <div className={styles.SubRow}>
+            <p className={styles.TSub}>Duration</p>
+            <p className={styles.TVal}>{formattedDuration}</p>
+          </div>
+          <div className={styles.SubRow}>
+            <p className={styles.TSub}>Completed Objectives</p>
+            <p className={styles.TVal}>{completedObjectives}</p>
+          </div>
+        </div>
+      );
+    });
 
   const renderTesters = () =>
     testers.map(tester => (
@@ -39,18 +71,7 @@ const Tests = ({ session }) => {
       <div className={styles.Container}>
         <div style={{ display: 'flex' }}>
           <div className={styles.Tests}>
-            <div className={styles.Test}>
-              <p className={styles.Dates}>Created [date]</p>
-              <p className={styles.User}>Test by [user]</p>
-              <div className={styles.SubRow}>
-                <p className={styles.TSub}>Duration</p>
-                <p className={styles.TVal}>1:20</p>
-              </div>
-              <div className={styles.SubRow}>
-                <p className={styles.TSub}>Completed Objectives</p>
-                <p className={styles.TVal}>3</p>
-              </div>
-            </div>
+            {renderTests()}
           </div>
           <div className={styles.Testers}>
             <div className={styles.THeader}>Testers</div>
