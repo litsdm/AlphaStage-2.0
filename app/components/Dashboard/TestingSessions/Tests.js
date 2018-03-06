2aebcef9aeee2120ca3f68b308e9d1c84b2d59bd
cdiezmoran <cdiezmoran@gmail.com>
2018-03-05 18:36:12 -0600

    Display session info other than tests

--- app/components/Dashboard/TestingSessions/Tests.js
+++ app/components/Dashboard/TestingSessions/Tests.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import uuid from 'uuid/v4';
+import { object } from 'prop-types';
+import styles from './Tests.scss';
 
-const Tests = () => (
-  <React.Fragment>
-    <p>Tests m8</p>
-  </React.Fragment>
-);
+const DEFAULT_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
+
+const Tests = ({ session }) => {
+  const { testers, maxTesters, rewardType, reward } = session;
+
+  const renderTesters = () =>
+    testers.map(tester => (
+      <div key={uuid()} className={styles.Tester}>
+        <img src={tester.profilePic || DEFAULT_IMAGE} alt="Tester profile pic" />
+        <p>{tester.username}</p>
+      </div>
+    ));
+
+  const infoRow = (subtitle, value) => (
+    <div className={styles.Row}>
+      <p className={styles.Subtitle}>{subtitle}</p>
+      <p className={styles.Value}>{value}</p>
+    </div>
+  );
+
+  return (
+    <React.Fragment>
+      <div className={styles.Container}>
+        {infoRow('Status', 'Active')}
+        {infoRow('Testers', `${testers.length} / ${maxTesters}`)}
+        {infoRow('Reward Type', rewardType)}
+        {
+          rewardType !== 'No Reward'
+            ? infoRow('Reward', reward)
+            : null
+        }
+      </div>
+      <div className={styles.Divider} />
+      <div className={styles.Container}>
+        <div style={{ display: 'flex' }}>
+          <div className={styles.Tests}>
+            <div className={styles.Test}>
+              test
+            </div>
+          </div>
+          <div className={styles.Testers}>
+            <div className={styles.THeader}>Testers</div>
+            <div className={styles.TList}>
+              {renderTesters()}
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+Tests.propTypes = {
+  session: object.isRequired
+};
 
 export default Tests;
