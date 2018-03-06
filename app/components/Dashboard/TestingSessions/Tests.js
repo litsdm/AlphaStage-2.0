3d7b621b1857aed143ad935ce5542a5693ef8a7b
cdiezmoran <cdiezmoran@gmail.com>
2018-03-06 16:43:05 -0600

    Display actual status on session detail

--- app/components/Dashboard/TestingSessions/Tests.js
+++ app/components/Dashboard/TestingSessions/Tests.js
@@ -10,6 +10,18 @@ const DEFAULT_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profi
 const Tests = ({ session }) => {
   const { testers, maxTesters, rewardType, reward, tests } = session;
 
+  const getStatus = () => {
+    const { startDate, endDate } = session;
+    const now = moment();
+
+    if (now.diff(startDate) < 0) {
+      return <span className={styles.Pending}>Pending <p>{` - Starts ${now.to(startDate)}`}</p></span>;
+    } else if (now.diff(startDate) >= 0 && now.diff(endDate) < 0) {
+      return <span className={styles.Active}>Active <p>{` - Ends ${now.to(endDate)}`}</p></span>;
+    }
+    return <span className={styles.Finished}>Finished <p>{` - ${now.to(endDate)}`}</p></span>;
+  };
+
   const formatDuration = (duration) => {
     const sec = parseInt(duration % 60, 10);
     const min = parseInt((duration / 60) % 60, 10);
@@ -51,14 +63,14 @@ const Tests = ({ session }) => {
   const infoRow = (subtitle, value) => (
     <div className={styles.Row}>
       <p className={styles.Subtitle}>{subtitle}</p>
-      <p className={styles.Value}>{value}</p>
+      <span className={styles.Value}>{value}</span>
     </div>
   );
 
   return (
     <React.Fragment>
       <div className={styles.Container}>
-        {infoRow('Status', 'Active')}
+        {infoRow('Status', getStatus())}
         {infoRow('Testers', `${testers.length} / ${maxTesters}`)}
         {infoRow('Reward Type', rewardType)}
         {
